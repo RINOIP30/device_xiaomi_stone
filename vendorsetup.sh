f450f736c7e247f498d0ded02b978aa4bc404915
kamikaonashi <thorschi93@gmx.at>
2023-09-30 20:17:42 +0200

    stone: drop dependencies and vendorsetup

--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-git clone --depth=1 https://github.com/Stone-Trees/device_xiaomi_sm6375-common.git -b lineage-20 device/xiaomi/sm6375-common
-
-git clone --depth=1 https://github.com/Stone-Trees/vendor_xiaomi.git vendor/xiaomi
-
-git clone --depth=1 https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b lineage-20 kernel/xiaomi/sm6375
-
-git clone --depth=1 https://github.com/Stone-Trees/xiaomi_stone-kernel.git device/xiaomi/stone-kernel
-
-git clone --depth=1 https://github.com/stone-devs/hardware_xiaomi hardware/xiaomi
