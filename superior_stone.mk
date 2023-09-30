84e55e433d371e78f54bda1ae79da4e02938d49b
kamikaonashi <thorschi93@gmx.at>
2023-09-30 20:16:47 +0200

    stone: adapt to SuperiorOS

--- superior_stone.mk
+++ superior_stone.mk
@@ -9,21 +9,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
-# Device props
-TARGET_SUPPORTS_BLUR := true
-TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_MATLOG := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+USE_MOTO_CALCULATOR := true
+TARGET_SUPPORTS_BLUR := true
+USE_MOTO_CLOCK := false
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+#BUILD_WITH_GAPPS := true
+USE_QUICKPIC := false
+USE_DUCKDUCKGO := false
 
-# Charging Animation
-TARGET_USE_PIXEL_CHARGER := true
+#WITH_GMS := true
+#WITH_GAPPS := true
+#TARGET_USE_GOOGLE_TELEPHONY := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_stone
+PRODUCT_NAME := superior_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
